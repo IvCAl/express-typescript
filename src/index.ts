700c2e829f6b6bf83f903f408b259950dea253ee
IvCAl <calivaivanalejandro@gmail.com>
2024-03-22 19:12:02 -0300

    intentando agregar el buscaminas

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import express from 'express'
+import 
+
 const app=express()
 app.use(express.json()) //middleware que transforma la req.body a un json
 
 const PORT=3000
 
+
+app.get('/buscaminas',(_req,res)=>{
+    res.send('Juego abierto')
+    console.log('Aqui deberia abrirse una ventana')
+})
 app.get('/ping',(_req,res) => {
     console.log('alguien hizo ping')
     res.send('pong')
