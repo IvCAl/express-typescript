683601c08adecbeb515fad18db98d4602f23628b
IvCAl <calivaivanalejandro@gmail.com>
2024-03-22 17:21:22 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+const app=express()
+app.use(express.json()) //middleware que transforma la req.body a un json
+
+const PORT=3000
+
+app.get('/ping',(_req,res) => {
+    console.log('alguien hizo ping')
+    res.send('pong')
+})
+
+app.listen(PORT, ()=>{
+    console.log(`El servidor esta en el puerto ${PORT}`)
+})
